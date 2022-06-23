4b37b34c3dc0a64b9ada6a7ca2fd4cb11b801435
Andre Brito <andre.tavares@prodam.am.gov.br>
2022-06-23 13:37:46 -0400

    first commit

---
+++ wp-content/plugins/al_patrocinadores_palestras/al_patrocinadores_palestras.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * Plugin Name: Patrocinadores Alura
+ * Description: Selecionar patrocinadores da palestra da Alura
+ * Version: 1.0.0
+ * Author: Rafael Nercessian
+ */
+
+if(!defined('ABSPATH')){
+	die;
+
+}
+//echo plugin_dir_path(__FILE__);
+
+require_once plugin_dir_path(__FILE__) . '/includes/al_patrocinadores_palestras_widget.php';
+
+
+
