d2acb1af334d1ac7a9b1c49781fcf2a44fa29d61
Andre Brito <atb.tavares@gmail.com>
2022-06-28 00:58:22 -0400

    Incluir campo endereço

--- wp-content/plugins/al_local_dia_palestra/includes/al_local_dia_palestra_settings.php
+++ wp-content/plugins/al_local_dia_palestra/includes/al_local_dia_palestra_settings.php
@@ -31,6 +31,7 @@ function al_local_dia_palestra_menu_pagina()
 add_action('admin_menu','al_local_dia_palestra_secao');
 function al_local_dia_palestra_secao()
 {
+	// Seção
 	add_settings_section(
 		'al_local_dia_palestra_secao',
 		'Configurações do local da palestra',
@@ -38,4 +39,33 @@ function al_local_dia_palestra_secao()
 		'local-palestra'
 	);
 
+	// Endereço
+	add_settings_field(
+		'al_local_dia_palestra_endereco',
+		'Endereço',
+		'al_local_dia_palestra_endereco',
+		'local-palestra',
+		'al_local_dia_palestra_secao'
+	);
+
+	register_setting(
+		'al_local_dia_palestra_settings', // option_group
+		'al_local_dia_palestra_endereco'  // option_name
+	);
 }
+
+function al_local_dia_palestra_campos_secao_detalhes()
+{
+	?>
+	<p>Insira os dados do encereço, da cidade e data da próxima palestra da Alura</p>
+	<?php	
+}
+
+function al_local_dia_palestra_endereco()
+{
+	?>
+	<input type="text" id="al_local_dia_palestra_endereco"
+		name="al_local_dia_palestra_endereco" required>
+	<?php
+}
+
