b3cab5c5105ed31a865d8f07b15fa205d32acd61
Andre Brito <atb.tavares@gmail.com>
2022-06-28 00:06:18 -0400

    Criar plugin de cadastro do local e data da palestra

---
+++ wp-content/plugins/al_local_dia_palestra/al_local_dia_palestra.php
@@ -0,0 +1,12 @@
+<?php
+/* Plugin Name: Cadastrar local e data da palestra
+ * Description: Plugin para cadastrar a data da próxima palestra realizada pela alura
+ * Version: 1.0.0
+ * Author: Andre Brito
+ */
+
+if (!defined('ABSPATH')){
+	die;
+}
+
+
